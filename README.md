66da1a24af24de305132d45cee5d3465bc5a6ddd
修昊 <github@svtter.cn>
2020-04-08 11:07:50 +0800

    update push script

---
+++ README.md
@@ -0,0 +1,8 @@
+# 每天一则机器学习
+
+pass
+
+
+## TODO
+
+- google analytics
---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'svtter'
+SITENAME = 'daily machine learning'
+SITEURL = 'https://svtter.cn/daily-ml'
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Shanghai'
+
+DEFAULT_LANG = 'Chinese (Simplified)'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Blog site', 'http://svtter.cn/'),
+         ('Python.org', 'http://python.org/'),)
+
+# Social widget
+SOCIAL = (('Github', 'https://github.com/svtter'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
